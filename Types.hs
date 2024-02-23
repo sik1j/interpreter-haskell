2bbf774799f39ae35b1dbf376df1450fe1f96db5
sik1j <karkisikij2@gmail.com>
2024-02-23 09:55:28 -0800

    Refactor expression and token types. Grammer rules no longer ambigous

--- Types.hs
+++ Types.hs
@@ -1,9 +1,7 @@
 module Types
   ( TokenType(..),
     Token(..),
-    Literal(..),
     Unary(..),
-    Operator(..),
     Expression(..)
   ) 
 where
@@ -38,51 +36,31 @@ data Token = Token
 instance Show Token where
   show Token {tokenType = tok, lexeme = lex, number = lit} = show tok ++ " " ++ lex ++ show lit
 
-data Literal = 
+data Expression = 
+  Equality Equality
+
+data Equality = 
+  Comparison Comparison | EqualEqual Comparison Comparison | NotEqual Comparison Comparison
+
+data Comparison =
+  Term Term | Greater Term Term | GreaterEqual Term Term | Less Term Term | LessEqual Term Term
+
+data Term = 
+  Factor Factor | Plus Factor Factor | MinusBinary Factor Factor
+
+data Factor = 
+  Unary Unary | Divide Unary Unary | Multiply Unary Unary
+
+data Unary = 
+  MinusUnary Unary | Bang Unary | 
+  Primary Primary
+
+data Primary = 
   Number Double |
   String String |
   Boolean Bool |
-  Nil
-
-data Unary = 
-  MinusUnary | Bang
-
-data Operator = 
-  Plus | MinusBinary | Star | Slash | Equal | NotEqual | Greater | GreaterEqual | Less | LessEqual
-
-data Expression = 
-  Literal Literal |
-  Unary Unary Expression |
-  Binary Expression Operator Expression |
+  Nil |
   Grouping Expression
-
-instance Show Literal where
-  show (Number num) = show num
-  show (String str) = show str
-  show (Boolean bool) = show bool
-  show Nil = "nil"
-
-instance Show Unary where
-  show MinusUnary = "-"
-  show Bang = "!"
-
-instance Show Operator where
-  show Plus = "+"
-  show MinusBinary = "-"
-  show Star = "*"
-  show Slash = "/"
-  show Equal = "=="
-  show NotEqual = "!="
-  show Greater = ">"
-  show GreaterEqual = ">="
-  show Less = "<"
-  show LessEqual = "<="
-
-instance Show Expression where
-  show (Literal literal) = show literal
-  show (Grouping expression) = parenthesize $ "group " ++ show expression
-  show (Unary unaryOp right) = parenthesize $ show unaryOp ++ " " ++ show right
-  show (Binary left operator right) = parenthesize $ show operator ++ " " ++ show left ++ " " ++ show right
 
 parenthesize :: String -> String 
 parenthesize str = "(" ++ str ++ ")"
--- main.hs
+++ main.hs
@@ -1,10 +1,10 @@
 import Types
 import Scanner
 import System.Environment
-main = do
-  let expr = Binary (Unary MinusUnary (Literal $ Number 123)) Star (Grouping (Literal $ Number 45.67))
+-- main = do
+  -- let expr = Binary (Unary MinusUnary (Literal $ Number 123)) Star (Grouping (Literal $ Number 45.67))
   -- let expr = Literal (NUMBER_LIT 1.0)
-  print expr
+  -- print expr
   -- args <- getArgs
   -- if null args
   --   then putStrLn "Please provide a .lox filename"
