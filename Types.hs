43a097a2f2ad6949323647a019da5b5d9a8c6c67
sik1j <karkisikij2@gmail.com>
2024-02-23 09:25:32 -0800

    Add new types and expressions to Types module. Expression printing set

--- Types.hs
+++ Types.hs
@@ -1,6 +1,10 @@
 module Types
   ( TokenType(..),
-    Token(..)
+    Token(..),
+    Literal(..),
+    Unary(..),
+    Operator(..),
+    Expression(..)
   ) 
 where
 
@@ -33,3 +37,52 @@ data Token = Token
 
 instance Show Token where
   show Token {tokenType = tok, lexeme = lex, number = lit} = show tok ++ " " ++ lex ++ show lit
+
+data Literal = 
+  Number Double |
+  String String |
+  Boolean Bool |
+  Nil
+
+data Unary = 
+  MinusUnary | Bang
+
+data Operator = 
+  Plus | MinusBinary | Star | Slash | Equal | NotEqual | Greater | GreaterEqual | Less | LessEqual
+
+data Expression = 
+  Literal Literal |
+  Unary Unary Expression |
+  Binary Expression Operator Expression |
+  Grouping Expression
+
+instance Show Literal where
+  show (Number num) = show num
+  show (String str) = show str
+  show (Boolean bool) = show bool
+  show Nil = "nil"
+
+instance Show Unary where
+  show MinusUnary = "-"
+  show Bang = "!"
+
+instance Show Operator where
+  show Plus = "+"
+  show MinusBinary = "-"
+  show Star = "*"
+  show Slash = "/"
+  show Equal = "=="
+  show NotEqual = "!="
+  show Greater = ">"
+  show GreaterEqual = ">="
+  show Less = "<"
+  show LessEqual = "<="
+
+instance Show Expression where
+  show (Literal literal) = show literal
+  show (Grouping expression) = parenthesize $ "group " ++ show expression
+  show (Unary unaryOp right) = parenthesize $ show unaryOp ++ " " ++ show right
+  show (Binary left operator right) = parenthesize $ show operator ++ " " ++ show left ++ " " ++ show right
+
+parenthesize :: String -> String 
+parenthesize str = "(" ++ str ++ ")"
--- main.hs
+++ main.hs
@@ -2,10 +2,13 @@ import Types
 import Scanner
 import System.Environment
 main = do
-  args <- getArgs
-  if null args
-    then putStrLn "Please provide a .lox filename"
-    else runFile $ head args
+  let expr = Binary (Unary MinusUnary (Literal $ Number 123)) Star (Grouping (Literal $ Number 45.67))
+  -- let expr = Literal (NUMBER_LIT 1.0)
+  print expr
+  -- args <- getArgs
+  -- if null args
+  --   then putStrLn "Please provide a .lox filename"
+  --   else runFile $ head args
 
 runFile :: String -> IO ()
 runFile filename = do
