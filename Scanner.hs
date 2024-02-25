eb604e41487f3ad53a441a932dcd0e03e25ce71a
sik1j <karkisikij2@gmail.com>
2024-02-24 20:59:10 -0800

    Refactor Scanner module to improve readability

--- Scanner.hs
+++ Scanner.hs
@@ -1,37 +1,42 @@
-module Scanner where
+module Scanner(
+  scanTokens 
+) where
 
 import Types (Token (..), TokenType (..))
 import Data.Char
 import Data.Maybe
 
-scanTokens :: Int -> String -> [Token]
-scanTokens line [] = [createToken EOF [] line Nothing]
-scanTokens line [x] = if isSpace x then scanTokens line [] else charToken line x : scanTokens line []
-scanTokens line code@(x : y : ys) = case x of
+scanTokens :: String -> [Token]
+scanTokens = scanTokens' 1
+
+scanTokens' :: Int -> String -> [Token]
+scanTokens' line [] = [createToken EOF [] line Nothing]
+scanTokens' line [x] = if isSpace x then scanTokens' line [] else charToken line x : scanTokens' line []
+scanTokens' line code@(x : y : ys) = case x of
   '!' -> if y == '=' then add2CharToken BANG_EQUAL else add1CharToken
   '=' -> if y == '=' then add2CharToken EQUAL_EQUAL else add1CharToken
   '<' -> if y == '=' then add2CharToken LESS_EQUAL else add1CharToken
   '>' -> if y == '=' then add2CharToken GREATER_EQUAL else add1CharToken
-  '/' -> if y == '/' then (scanTokens line . consumeComment) ys else add1CharToken
+  '/' -> if y == '/' then (scanTokens' line . consumeComment) ys else add1CharToken
   '"' -> addString (y : ys)
   _
-    | isSpace x && x /= '\n' -> scanTokens line (y : ys)
-    | x == '\n' -> scanTokens (line + 1) (y : ys)
+    | isSpace x && x /= '\n' -> scanTokens' line (y : ys)
+    | x == '\n' -> scanTokens' (line + 1) (y : ys)
     | isDigit x -> addNumber code
     | isAlphaOrUnderScore x || x == '_' -> addIdentifier code
     | otherwise -> add1CharToken
   where
-    add2CharToken tok = createToken tok [x, y] line Nothing : scanTokens line ys
-    add1CharToken = charToken line x : scanTokens line (y : ys)
+    add2CharToken tok = createToken tok [x, y] line Nothing : scanTokens' line ys
+    add1CharToken = charToken line x : scanTokens' line (y : ys)
     consumeComment = dropWhile (/= '\n')
 
     addString code = if null rest
         then error ("Line " ++ show line ++ ": unterminated string at line")
-        else createToken STRING str line Nothing : scanTokens (line + length (filter (=='\n') str)) (tail rest)
+        else createToken STRING str line Nothing : scanTokens' (line + length (filter (=='\n') str)) (tail rest)
       where
         (str, rest) = span (/= '"') code
 
-    addNumber code = createToken NUMBER strNum line (Just num) : scanTokens line rest
+    addNumber code = createToken NUMBER strNum line (Just num) : scanTokens' line rest
       where
         (preDecimal, other) = span isDigit code
         (postDecimal, rest) = case other of
@@ -43,7 +48,7 @@ scanTokens line code@(x : y : ys) = case x of
         num = stringToDouble strNum
 
     isAlphaOrUnderScore x = isAlpha x || x == '_'
-    addIdentifier code = createToken (fromMaybe IDENTIFIER (keywordType iden)) iden line Nothing : scanTokens line rest
+    addIdentifier code = createToken (fromMaybe IDENTIFIER (keywordType iden)) iden line Nothing : scanTokens' line rest
       where
         (iden, rest) = span (\x -> isAlphaOrUnderScore x || isDigit x) code
         keywordType str = case str of
